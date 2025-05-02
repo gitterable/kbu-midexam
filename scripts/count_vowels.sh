46c3c9925360579416da7d817da75492befb2cb9
gitterable <tjdqudcks0424@naver.com>
2025-05-02 09:45:45 +0900

    [Q1] fix: count vowels in sample.txt case-insensitively

--- scripts/count_vowels.sh
+++ scripts/count_vowels.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-count=$(grep -o '[aeiou]' sample.txt | wc -w)
-echo "Total vowels: $count"
+count=$(tr -cd 'aeiouAEIOU' < scripts/sample.txt | wc -c)
+echo "Number of vowels:$count"
